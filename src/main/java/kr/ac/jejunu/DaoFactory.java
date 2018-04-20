d06aa589750f0de29615faf10e4e8306c697b160
syoung <syoung1394@naver.com>
2018-04-20 11:48:31 +0900

    exam13

--- src/main/java/kr/ac/jejunu/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -22,11 +23,11 @@ public class DaoFactory {
 
     @Bean
     public ProductDao productDao()  {
-        return new ProductDao(jdbcContext());
+        return new ProductDao(jdbcTemplate());
     }
     @Bean
-    public JdbcContext jdbcContext() {
-        return new JdbcContext(dataSource());
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 
     @Bean
--- src/main/java/kr/ac/jejunu/ProductDao.java
+++ src/main/java/kr/ac/jejunu/ProductDao.java
@@ -1,36 +1,60 @@
 package kr.ac.jejunu;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import java.sql.*;
 
 public class ProductDao  {
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
-    public ProductDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public ProductDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public Product get(Long id) throws SQLException {
         String sql = "select * from product where id = ?";
         Object []params=new Object[]{id};
-        return jdbcContext.queryForObject(sql, params);
+        try {
+            return jdbcTemplate.queryForObject(sql, params,(rs, rowNum) -> {
+                Product product=new Product();
+                product.setPrice(rs.getInt("price"));
+                product.setTitle(rs.getString("title"));
+                product.setId(rs.getLong("id"));
+                return product;
+            });
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     public Long insert(Product product) throws SQLException{
         String sql = "insert into product(title, price) values(?, ?)";
         Object []params=new Object[]{product.getTitle(), product.getPrice()};
-        return jdbcContext.insert(sql, params);
+        KeyHolder keyHolder=new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for(int i=0; i<params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        },keyHolder);
+        return keyHolder.getKey().longValue();
     }
 
     public void update(Product product) throws SQLException {
         String sql = "update product set title=?, price=? where id=?";
         Object []params=new Object[]{product.getTitle(), product.getPrice(), product.getId()};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Long id) throws SQLException {
         String sql = "delete from product where id=?";
         Object []params=new Object[]{id};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
 
